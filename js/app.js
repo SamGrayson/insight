1ba5e42da405de94715a965b04de20463d72ce07
Sam Grayson <samuel.c.grayson17@gmail.com>
2016-04-24 14:54:26 -0400

    close menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation();
+$(document).ready(function(){
+  $(document).foundation();
+
+  // CLOSE MENU
+  $('.insight-dropdown').on('click', function(e){
+    e.preventDefault()
+    if($('.insight-dropdown-open').hasClass('js-dropdown-active') === true)
+     $('.insight-dropdown-open').removeClass('js-dropdown-active')
+  })
+})
